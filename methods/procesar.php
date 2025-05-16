163dedf7846f7dacba41d58f56b101d5f18e6ae7
VictorPerezSTI <108298032+VictorPerezSTI@users.noreply.github.com>
2025-05-16 17:50:35 -0600

    pdf con logo

--- methods/procesar.php
+++ methods/procesar.php
@@ -15,7 +15,7 @@ $mensaje = $_POST['mensaje '];
 class PDF extends FPDF {
     function Header() {
         // Logo a la izquierda
-        //$this->Image('../img/logoKioxHorizontal.svg', 10, 10, 30); // x, y, width
+        $this->Image('../img/logoKiox.png', 10, 10, 30); // x, y, width
 
         // Fuente para encabezado
         $this->SetFont('Arial', '', 12);
@@ -29,7 +29,7 @@ class PDF extends FPDF {
         $this->SetFont('Arial', 'B', 12);
         $this->Cell(0, 10, $fisioterapeuta, 0, 1, 'C');
         $this->SetFont('Arial', '', 11);
-        //$this->Cell(0, 6, 'Cédula Profesional: ' + $cedula, 0, 1, 'C');
+        $this->Cell(0, 6, 'Cédula Profesional: ' + $cedula, 0, 1, 'C');
         $this->Cell(0, 6, 'Dirección: Patricio Sanz 442, \r\n Col. Del Valle Norte C.P.03103', 0, 1, 'C');
 
         // Espacio después del encabezado
@@ -38,7 +38,7 @@ class PDF extends FPDF {
 }
 
 // Crear PDF
-$pdf = new PDF();
+$pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial', '', 12);
 $pdf->MultiCell(0, 10, $mensaje);
