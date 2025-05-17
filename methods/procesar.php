170c04b4bd6eb541c581ab736defe6c61ea05232
VictorPerezSTI <108298032+VictorPerezSTI@users.noreply.github.com>
2025-05-16 18:10:14 -0600

    acentos

--- methods/procesar.php
+++ methods/procesar.php
@@ -30,7 +30,7 @@ class PDF extends FPDF {
         //$this->Cell(0, 10, utf8_decode($fisioterapeuta), 0, 1, 'C');
         $this->SetFont('Arial', '', 11);
         //$this->Cell(0, 6, utf8_decode('Cédula Profesional: ' + $cedula), 0, 1, 'C');
-        //$this->Cell(0, 6, utf8_decode('Dirección: Patricio Sanz 442, \r\n Col. Del Valle Norte C.P.03103'), 0, 1, 'C');
+        $this->Cell(0, 6, utf8_decode('Dirección: Patricio Sanz 442, \r\n Col. Del Valle Norte C.P.03103'), 0, 1, 'C');
         $this->Cell(0, 6, 'Hola', 0, 1, 'C');
 
         // Espacio después del encabezado
