a52c412c8903badd36c59b9ad839cdd529b32e67
VictorPerezSTI <108298032+VictorPerezSTI@users.noreply.github.com>
2025-06-13 13:15:28 -0600

    correcion pdf

--- methods/envioFormatos.js
+++ methods/envioFormatos.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
 
                 const formData = $form.serialize(); 
                 $.ajax({
-                    url: "../methods/procesar.php?kvs=3.9",
+                    url: "../methods/procesar.php?kvs=3.10",
                     type: "POST",
                     data: formData,
                     success: function (respuesta) {
